946ce8b7a58f6173832499611132e67f5a13cb04
Cristoffer Pogan <cristoffer_pogan@econeteditora.com.br>
2024-04-22 08:54:21 -0300

    TypeScript parte 2: avançando na linguagem (01.Elaborando uma solução de view)

--- TypeScript/app/views/negociacoes-view.ts
+++ TypeScript/app/views/negociacoes-view.ts
@@ -19,9 +19,9 @@ export class NegociacoesView {
             </thead>
             <tbody>
                 ${model.lista().map(negociacao => {
-                    return `
+                        return `
                         <tr>
-                            <td>?</td>
+                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
                         </tr>
@@ -34,7 +34,6 @@ export class NegociacoesView {
 
     update(model: Negociacoes): void {
         const template = this.template(model);
-        console.log(template);
         this.elemento.innerHTML = template;
     }
 }
